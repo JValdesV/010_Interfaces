13a2101804a38a2dc1f68e9fbabf92caac41ef15
Javier Valdés <j.valdes.tec@gmail.com>
2023-07-02 00:13:34 -0400

    Interfaces.

---
+++ src/Clases/Impresora.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public class Impresora implements Imprimible{
+
+	@Override
+	public void imprimir() {
+		System.out.println("Imprimiendo....");
+	}
+
+}
---
+++ src/Principal/Main.java
@@ -0,0 +1,14 @@
+package Principal;
+
+import Clases.Impresora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Impresora impresora = new Impresora();
+		impresora.imprimir();
+		
+	}
+
+}
